9264e315f56bf880a709f798ceba4ee614700deb
Jan Pazdziora <jpx-perl@adelton.com>
1998-01-07 18:56:39 +0000

    Import of JANPAZ/GD-Latin2-0.5 from CPAN.
    gitpan-cpan-distribution: GD-Latin2
    gitpan-cpan-version:      0.5
    gitpan-cpan-path:         JANPAZ/GD-Latin2-0.5.tar.gz
    gitpan-cpan-author:       JANPAZ
    gitpan-cpan-maturity:     released

---
+++ Latin2.pm
@@ -0,0 +1,73 @@
+=head1 NAME
+
+GD::Latin2 - ISO-8859-2 support for GD module
+
+=head1 SYNOPSIS
+
+If you have successfully aplied this patch and reinstalled GD, you can
+call string with parameters containing ISO-Latin-2 characters and they
+will be correctly displayed.
+
+=head1 DESCRIPTION
+
+This is a patch that replaces standard ASCII-only font in libgd, used
+by GD module, by those with full 256 char Latin2 support. In order to
+put new fonts into the library, the source has to be changed and the
+library recompiled. That's why you need to have untarred GD library
+ready before aplying this patch.
+
+=head1 INSTALLATION
+
+This is my attempt to make the patch easily installable. However, if
+may fail on your configuration, but do not worry -- you can patch the
+GD yourself, see below.
+
+Unpack this distribution next to unpacked GD distribution. Then run
+
+	perl Makefile.PL
+	make
+	make install
+
+You will be prompted for a directory containing the untarred GD
+distribution. Then the files containing the fonts are replaced with
+those with ISO-8859-2. Also, one minor patch is aplied to the libgd.c
+in order to make it 8-bit clean. And, the fifth test output gif (that
+contains some texts) is also replaced, so that the tests run OK.
+
+You can of course do all this by hand: the fonts are in the fonts
+directory, you should put them into the libgd directory in GD
+distribution. The apply the patch in file libgd.diff to libgd.c file
+in GD distribution.
+
+=head1 OTHER CHARSETS
+
+In this package, I have also included my little script B<bdftogd> that
+creates gd font definitions from those in bdf file format. So you can
+run you favorite ISO-8859-1 or so fonts through it and make use of
+them.
+
+=head1 BUGS
+
+You have to reinstal this patch whenever you upgrade your GD package.
+
+The small font has changed from 6x12 to 6x13 in ISO-8859-2 version.
+
+The tiny font is really unreadable -- I will try to find some better.
+
+The bdftogd convertor is really a quick hack. It works on bdf's that
+my fontserver generated, but I haven't studied the bdf definition -- I
+simply made it work. You are welcome to find problems and send me
+patches to make it more general.
+
+=head1 VERSION
+
+0.5
+
+=head1 AUTHOR
+
+Jan Pazdziora, adelton@fi.muni.cz
+
+http://www.fi.muni.cz/~adelton/
+
+at Faculty of Informatics, Masaryk University in Brno, Czech Republic
+
---
+++ fonts/gdfontt.h
@@ -0,0 +1,20 @@
+
+#ifndef _GDFONTT_H_
+#define _GDFONTT_H_ 1
+
+/*
+	This is a header file for gd font, generated using
+	bdftogd version 0.5 by Jan Pazdziora, adelton@fi.muni.cz
+	from bdf font
+	-misc-fixed-medium-r-normal-sans-8-77-75-75-c-35-iso8859-2
+	at Wed Jan  7 15:34:50 1998.
+	No copyright info was found in the original bdf.
+ */
+
+
+#include "gd.h"
+
+extern gdFontPtr gdFontTiny;
+
+#endif
+
